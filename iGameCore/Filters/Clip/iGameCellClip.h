81a82346e4474f7327358d29f33a5414b2c3fb9a
xixiaomo8 <2436125402@qq.com>
2024-10-07 22:15:01 +0800

    更新切割

--- iGameCore/Filters/Clip/iGameCellClip.h
+++ iGameCore/Filters/Clip/iGameCellClip.h
@@ -5,11 +5,82 @@ IGAME_NAMESPACE_BEGIN
 
 namespace CellClip {
 
+	struct TETRA_CLIP {
+		int clip[7];
+	};
+	static TETRA_CLIP tetraCases[] = {
+	  { { 0, 0, 0, 0, 0, 0, 0 } },        // 0
+	  { { 4, 0, 3, 2, 100, 0, 0 } },      // 1
+	  { { 4, 0, 1, 4, 101, 0, 0 } },      // 2
+	  { { 6, 101, 1, 4, 100, 2, 3 } },    // 3
+	  { { 4, 1, 2, 5, 102, 0, 0 } },      // 4
+	  { { 6, 102, 5, 1, 100, 3, 0 } },    // 5
+	  { { 6, 102, 2, 5, 101, 0, 4 } },    // 6
+	  { { 6, 3, 4, 5, 100, 101, 102 } },  // 7
+	  { { 4, 3, 4, 5, 103, 0, 0 } },      // 8
+	  { { 6, 103, 4, 5, 100, 0, 2 } },    // 9
+	  { { 6, 103, 5, 3, 101, 1, 0 } },    // 10
+	  { { 6, 100, 101, 103, 2, 1, 5 } },  // 11
+	  { { 6, 2, 102, 1, 3, 103, 4 } },    // 12
+	  { { 6, 0, 1, 4, 100, 102, 103 } },  // 13
+	  { { 6, 0, 3, 2, 101, 103, 102 } },  // 14
+	  { { 4, 100, 101, 102, 103, 0, 0 } } // 15
+	};
 
-	void Clip(Tetra::Pointer cell,float cellValues,CellArray::Pointer connectivity,
-		AttributeSet::Pointer inData, AttributeSet::Pointer outData,igIndex cellId)
+
+	static void Clip(Cell::Pointer cell, float* cellValues, Points::Pointer points, CellArray::Pointer connectivity,UnsignedIntArray::Pointer types,
+		AttributeSet::Pointer inData, AttributeSet::Pointer outData, igIndex cellId)
 	{
+		int MASK[4] = { 1,2,4,8 };
+		int i, j, CaseIndex = 0;
+		igIndex pId = 0;
+		igIndex vhs[IGAME_CELL_MAX_SIZE];
+		igIndex vcnt = 0;
+		const int* vert = nullptr;
+		igIndex vh1 = 0, vh2 = 0;
+		Point v1, v2, p;
+		double deltaValue = 0.0, t = 0.0;
+		for (i = 0; i < 4; i++) {
+			if (cellValues[i] <= 0.0) {
+				CaseIndex |= MASK[i];
+			}
+		}
+		auto ClipData = (tetraCases + CaseIndex)->clip;
+		for (i = 1; i <= ClipData[0]; i++) {
 
+			if (ClipData[i] >= 100) {
+				pId = ClipData[i] - 100;
+				vhs[i - 1] = cell->GetPointId(pId);
+			}
+			else {
+				vert = Tetra::edges[ClipData[i]];
+				deltaValue = cellValues[vert[1]] - cellValues[vert[0]];
+				if (deltaValue > 0) {
+					vh1 = vert[0];
+					vh2 = vert[1];
+				}
+				else {
+					vh2 = vert[0];
+					vh1 = vert[1];
+					deltaValue = -deltaValue;
+				}
+				t = (deltaValue == 0.0 ? 0.0 : -cellValues[vh1] / deltaValue);
+
+				v1 = cell->GetPoint(vh1);
+				v2 = cell->GetPoint(vh2);
+				p = v1 + (v2 - v1) * t;
+				points->AddPoint(p);
+				vhs[i - 1] = points->GetNumberOfPoints()-1;
+			}
+		}
+		if (ClipData[0] == 4) {
+			connectivity->AddCellIds(vhs, 4);
+			types->AddValue(IG_TETRA);
+		}
+		else {
+			connectivity->AddCellIds(vhs, 6);
+			types->AddValue(IG_PRISM);
+		}
 	}
 }
 
--- iGameCore/Filters/Clip/iGameUnstructuredMeshClip.h
+++ iGameCore/Filters/Clip/iGameUnstructuredMeshClip.h
@@ -19,9 +19,17 @@ public:
 		return (this->m_Normal[0] * (x[0] - this->m_Origin[0]) + this->m_Normal[1] * (x[1] - this->m_Origin[1]) +
 			this->m_Normal[2] * (x[2] - this->m_Origin[2]));
 	}
-	float GetCutValue(float x0,float x1,float x2) {
-		return (this->m_Normal[0] * (x0 - this->m_Origin[0]) + this->m_Normal[1] * (x1- this->m_Origin[1]) +
+	float GetCutValue(float x0, float x1, float x2) {
+		return (this->m_Normal[0] * (x0 - this->m_Origin[0]) + this->m_Normal[1] * (x1 - this->m_Origin[1]) +
 			this->m_Normal[2] * (x2 - this->m_Origin[2]));
+	}
+	void SetPlane(float n[3], float o[3]) {
+		m_Normal[0] = n[0];
+		m_Normal[1] = n[1];
+		m_Normal[2] = n[2];
+		m_Origin[0] = o[0];
+		m_Origin[1] = o[1];
+		m_Origin[2] = o[2];
 	}
 protected:
 	UnstructuredMeshClip();
@@ -29,7 +37,7 @@ protected:
 	float m_CutPlane[4];
 	float m_Normal[3];
 	float m_Origin[3];
-	UnstructuredMesh::Pointer m_UnstructuredMesh{nullptr};
+	UnstructuredMesh::Pointer m_UnstructuredMesh{ nullptr };
 };
 IGAME_NAMESPACE_END
 #endif
