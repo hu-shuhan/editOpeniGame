dfa7fa3f1fea6023d1d6417d8d4880fd1d3e13fb
xixiaomo8 <2436125402@qq.com>
2024-10-12 19:14:49 +0800

    更新切片ui

--- Qt/src/IQWidgets/igQtModelClipWidget.cpp
+++ Qt/src/IQWidgets/igQtModelClipWidget.cpp
@@ -1,20 +1,30 @@
 #include "IQWidgets/igQtModelClipWidget.h"
 
 igQtModelClipWidget::igQtModelClipWidget(QWidget* parent)
-    : QWidget(parent)/*, ui(new Ui::ScalarView) */{
+	: QWidget(parent)/*, ui(new Ui::ScalarView) */ {
 
-	m_Clipper=iGame::ModelClip::New();
+	m_Clipper = iGame::ModelClip::New();
 }
 
 
-void igQtModelClipWidget::SetPlane(float n[3], float o[3])
+void igQtModelClipWidget::SetPlane(float o[3], float n[3])
 {
-	m_Clipper->SetPlane(n,o);
+	m_Clipper->SetPlane(o, n);
 }
 
 void igQtModelClipWidget::UpdatePlane()
 {
+	float o[3] = { 0 };
+	float n[3] = { 0 };
 
+	o[0] = .0;
+	o[1] = .0;
+	o[2] = .0;
+	n[0] = .0;
+	n[1] = .0;
+	n[2] = .0;
+
+	m_Clipper->SetPlane(o,n);
 }
 void igQtModelClipWidget::SetIsSlice(bool s)
 {
