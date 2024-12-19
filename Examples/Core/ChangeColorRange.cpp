ab920268a3941b30f8a4a73207e67b707a3ea986
xixiaomo8 <2436125402@qq.com>
2024-12-19 22:31:17 +0800

    update

--- Examples/Core/ChangeColorRange.cpp
+++ Examples/Core/ChangeColorRange.cpp
@@ -5,43 +5,51 @@
 #include <iGameFileIO.h>
 
 int main() {
-    /* 创建场景*/
-    auto scene = iGame::Scene::New();
-    const std::string fileName = "./Models/Tet_Plane.vtk";
+	/* 创建场景*/
+	auto scene = iGame::Scene::New();
+	const std::string fileName = "./Models/Tet_Plane.vtk";
 
-    iGame::DataObject::Pointer obj = iGame::FileIO::ReadFile(fileName);
-    if (obj != nullptr) {
-        scene->AddModel(obj);
-    }
-    else {
-        std::cout << "Read ERROR!\n";
-        return -1;
-    }
-    auto drawObj=DynamicCast<iGame::DrawObject>(obj);
-    if (drawObj == nullptr) {
-        std::cout << "Could not render this object\n";
-        return -1;
-    }
-    auto mapper = drawObj->GetColorMapper();
-    //云图展示，第一个参数为scene，第二个参数是云图数据所在的index，
-    //第三个参数是展示数据的维度，-1表示长度，0表示第0维，默认为-1
-    drawObj->ViewCloudPicture(scene, 0);
-    //转化为渲染数据
-    drawObj->ConvertToDrawableData();
-    //自定义range
-    mapper->SetRange(0.3, 0.6);
-    //更新mapper的状态
-    mapper->Modified();
-    scene->Update();
+	iGame::DataObject::Pointer obj = iGame::FileIO::ReadFile(fileName);
+	if (obj != nullptr) {
+		scene->AddModel(obj);
+	}
+	else {
+		std::cout << "Read ERROR!\n";
+		return -1;
+	}
+	auto drawObj = DynamicCast<iGame::DrawObject>(obj);
+	if (drawObj == nullptr) {
+		std::cout << "Could not render this object\n";
+		return -1;
+	}
+	//云图展示，第一个参数为scene，第二个参数是云图数据所在的index，
+	//第三个参数是展示数据的维度，-1表示长度，0表示第0维，默认为-1
+	drawObj->ViewCloudPicture(scene, 0);
+	//转化为渲染数据
+	drawObj->ConvertToDrawableData();
+	auto mapper = drawObj->GetColorMapper();
+	float rgb[3] = { 0,0,0 };
+	float value = 0.4;
+	//初始range下的mapper映射0.4的value得到的rgb
+	mapper->MapColor(value, rgb);
+	std::cout << rgb[0] << ' ' << rgb[1] << ' ' << rgb[2] << '\n';
+	//自定义range
+	mapper->SetRange(0.3, 0.6);
+	//修改后range下的mapper映射0.4的value得到的rgb
+	mapper->MapColor(value, rgb);
+	std::cout << rgb[0] << ' ' << rgb[1] << ' ' << rgb[2] << '\n';
+	//更新mapper的状态
+	mapper->Modified();
+	scene->Update();
 
-    /* 启动窗口设置*/
-    iGame::RenderWindow::Pointer window = iGame::RenderWindow::New();
-    window->SetSize(1920, 1080);
-    window->SetScene(scene);
-    auto basicInteractor = iGame::Interactor::New();
-    basicInteractor->Initialize(scene);
-    window->SetInteractor(basicInteractor);
-    window->Show();
+	/* 启动窗口设置*/
+	iGame::RenderWindow::Pointer window = iGame::RenderWindow::New();
+	window->SetSize(1920, 1080);
+	window->SetScene(scene);
+	auto basicInteractor = iGame::Interactor::New();
+	basicInteractor->Initialize(scene);
+	window->SetInteractor(basicInteractor);
+	window->Show();
 
-    return 0;
+	return 0;
 }
