24a651f8489de30fd39dee7908118dc68cfc69a6
mky8812 <2819787107@qq.com>
2025-02-11 14:20:29 +0800

    update.

--- main.cpp
+++ main.cpp
@@ -18,34 +18,6 @@
 //#include <iGameRenderWindow.h>
 //#include <iGameInteractor.h>
 int main(int argc, char* argv[]) {
-//    using namespace iGame;
-//    /* init scene*/
-//    auto scene = iGame::Scene::New();
-//    /* Read the file Test and put it into the scene */
-//    const std::string fileName = "../Examples/Models/StreamTest.vtk";
-//    iGame::DataObject::Pointer obj = iGame::FileIO::ReadFile(fileName);
-//    DynamicCast<iGame::DrawObject>(obj)->AddViewStyle(IG_WIREFRAME);
-//    if(obj != nullptr){
-//        scene->AddModel(obj);
-//    } else {
-//        std::cout << "Read ERROR!\n";
-//    }
-//
-//    /* Launch window Settings */
-//    iGame::RenderWindow::Pointer window = iGame::RenderWindow::New();
-//    window->SetSize(1920, 1080);
-//    window->SetScene(scene);
-//
-//    auto interactor = iGame::Interactor::New();
-//    interactor->Initialize(scene);
-//    interactor->CreateDefaultStyle();
-//    window->SetInteractor(interactor);
-//
-//    /* show single window */
-//    window->Show();
-//
-//    return 0;
-
      Q_INIT_RESOURCE(iGameQtMainWindow);
      QCoreApplication::setAttribute(Qt::AA_EnableHighDpiScaling); // 窗口高分辨率支持
      QGuiApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);     // 图标高分辨率支持
@@ -81,40 +53,5 @@ int main(int argc, char* argv[]) {
      a.exec();
      return 0;
 
-
-//    /* 创建场景*/
-//    auto scene = iGame::Scene::New();
-//    const std::string fileName = "C:\\Users\\m_ky\\Desktop\\Resource\\xml\\pvd\\CAD11\\_frames.pvd";
-//    iGame::DataObject::Pointer obj = iGame::FileIO::ReadFile(fileName);
-////    auto obj = reader->ReadOdbFirstFrameMesh(fileName);
-//    if (obj == nullptr) {
-//        std::cout << "Read ERROR!\n";
-//    } else {
-//        scene->AddModel(obj);
-//    }
-//    /* Launch window Settings */
-//    iGame::RenderWindow::Pointer window = iGame::RenderWindow::New();
-//    window->SetSize(1920, 1080);
-//    window->SetScene(scene);
-//    auto interactor = iGame::Interactor::New();
-//    interactor->Initialize(scene);
-//    interactor->CreateDefaultStyle();
-//    window->SetInteractor(interactor);
-//
-//    std::cout << "attr num : " << obj->GetAttributeSet()->GetNumberOfAttributes() << '\n';
-//    /* Reset DataObject's magnitude dimension to Default.*/
-//    obj->GetAttributeSet()->GetAttribute(0).GetDataRange()->SetValue(0, DBL_MAX);
-//    obj->GetAttributeSet()->GetAttribute(0).GetDataRange()->SetValue(1, DBL_MIN);
-//    /* Recollect subdata object's MIN/MAX DataRange to update the current dataObject's data range. */
-//    obj->ReCollectSubDataObjectDataRange();
-//    /* Update Sub data Object's data range to the parent data range. */
-//    obj->UpdateSubDataObjectDataRange();
-//    DynamicCast<iGame::DrawObject>(obj)->ViewCloudPicture(scene, 0);
-//
-//    /* show single window */
-//    window->Show();
-//
-//
-//    return 0;
 
 }
