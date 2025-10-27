7b1e7e8e5826295dadf35e7dc78a6682740b7aed
苏穆泽. <1319406332@qq.com>
2025-10-27 21:50:15 +0800

    添加多块模型树ui

--- iGameCore/IO/VTK XML/iGameVTMReader.cpp
+++ iGameCore/IO/VTK XML/iGameVTMReader.cpp
@@ -8,9 +8,10 @@
  */
 
 #include "iGameVTMReader.h"
-#include <tinyxml2.h>
+#include "VTK/iGameVTKReader.h"
 #include "iGameVTSReader.h"
 #include "iGameVTUReader.h"
+#include <tinyxml2.h>
 
 
 IGAME_NAMESPACE_BEGIN
@@ -24,43 +25,55 @@ bool iGameVTMReader::Parsing() {
 
     std::vector<DataObject::Pointer> overall_multiBlock;
     DataObject::Pointer newObj;
-    while(BlockElem)
-    {
+    while (BlockElem) {
         elem = BlockElem->FirstChildElement();
 
-        DataObject::Pointer curMultiBlock = DataObject::New();
-        while(elem)
-        {
+        // 因为Scene中都用到了DrawObject的属性，后续多块要特殊处理
+        // DataObject::Pointer curMultiBlock = DataObject::New();
+        DataObject::Pointer curMultiBlock = DrawObject::New();
+
+        while (elem) {
             // TODO: Not finish Reading the Nested block.
-//            std::string elemType(elem->Value());
+            //            std::string elemType(elem->Value());
 
             existAttribute = elem->Attribute("file");
-            if(existAttribute)
-            {
+            if (existAttribute) {
                 std::string fileName(existAttribute);
                 std::string fileSuffix;
                 const char* pos = strrchr(fileName.data(), '.');
-                if (pos != nullptr)
-                {
-                    const char *fileEnd = fileName.data() + fileName.size();
+                if (pos != nullptr) {
+                    const char* fileEnd = fileName.data() + fileName.size();
                     fileSuffix = std::string(pos + 1, fileEnd);
                 }
-//                std::cout << fileDir + std::string(existAttribute) << '\n';
-                if(fileSuffix == "vts"){
+                if (fileSuffix == "vts") {
                     iGameVTSReader::Pointer rd = iGameVTSReader::New();
                     rd->SetFilePath(fileDir + std::string(existAttribute));
                     rd->Execute();
                     newObj = rd->GetOutput();
-                }
-                else if(fileSuffix == "vtu"){
+                } else if (fileSuffix == "vtu") {
                     iGameVTUReader::Pointer rd = iGameVTUReader::New();
                     rd->SetFilePath(fileDir + std::string(existAttribute));
                     rd->Execute();
                     newObj = rd->GetOutput();
+                } else if (fileSuffix == "vtk") {
+                    VTKReader::Pointer rd = VTKReader::New();
+                    rd->SetFilePath(fileDir + std::string(existAttribute));
+                    rd->Execute();
+                    newObj = rd->GetOutput();
+                } else {
+                    newObj = nullptr;
                 }
-            }
-            if(newObj){
-                curMultiBlock->AddSubDataObject(newObj);
+
+                if (newObj) {
+                    // Set the sub-data object's name to the file's base name (without extension)
+                    std::string baseName = fileName;
+                    size_t slashPos = baseName.find_last_of("/\\");
+                    if (slashPos != std::string::npos) baseName = baseName.substr(slashPos + 1);
+                    size_t dotPos = baseName.find_last_of('.');
+                    if (dotPos != std::string::npos) baseName = baseName.substr(0, dotPos);
+                    newObj->SetName(baseName);
+                    curMultiBlock->AddSubDataObject(newObj);
+                }
             }
             elem = elem->NextSiblingElement();
         }
@@ -70,14 +83,14 @@ bool iGameVTMReader::Parsing() {
     }
 
 
-    if(overall_multiBlock.empty()) {
+    if (overall_multiBlock.empty()) {
         return false;
-    } else if(overall_multiBlock.size() == 1) {
+    } else if (overall_multiBlock.size() == 1) {
         parseData = overall_multiBlock.front();
         return true;
     } else {
         DataObject::Pointer rootDataObject = DataObject::New();
-        for(const auto& mp : overall_multiBlock) rootDataObject->AddSubDataObject(mp);
+        for (const auto& mp: overall_multiBlock) rootDataObject->AddSubDataObject(mp);
 
         parseData = rootDataObject;
         return true;
