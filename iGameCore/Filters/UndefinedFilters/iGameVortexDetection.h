6c766e256a99977d267e861d84fd53cbef22af8f
Sumzeek <1319406332@qq.com>
2025-11-09 21:48:43 +0800

    update  libtorch_CUDA

--- iGameCore/Filters/UndefinedFilters/iGameVortexDetection.h
+++ iGameCore/Filters/UndefinedFilters/iGameVortexDetection.h
@@ -45,7 +45,7 @@ public:
     torch::Tensor sigmoid(const torch::Tensor& x);
 
     torch::Tensor run_prediction_on_block(const torch::Tensor& grid_tensor, const std::string& model_path,
-                                          const torch::jit::script::Module& model);
+                                          torch::jit::script::Module& model);
     // torch::Tensor run_prediction_on_block(const torch::Tensor& grid_tensor,torch::jit::script::Module& model);
 
     std::vector<torch::Tensor> extract_patches(const torch::Tensor& padded, int patch_size, int stride);
@@ -53,6 +53,9 @@ public:
     torch::Tensor _hann3d(int patch_size);
 
     std::tuple<torch::Tensor, int, int, int> pad_tensor(const torch::Tensor& grid_tensor, int patch_size);
+
+    torch::Tensor extract_patches_gpu_batched(
+    const torch::Tensor& padded, int patch_size, int stride);
 
     torch::Tensor padTensor(const torch::Tensor& tensor, int pad_z, int pad_y, int pad_x);
     Vector3f nearestVector(const Vector3f& pos, const std::vector<Vector3f>& gridPoints,
@@ -71,11 +74,13 @@ public:
     double compute_percentile_cell_volume(const std::vector<Vector3f>& points,
                                                     const std::vector<Volume*>& cells,double percentile);
 
-    std::tuple<torch::Tensor, Eigen::Vector3f> process_blocks(const std::vector<Vector3f>& gridPoints,
+    std::tuple<torch::Tensor, Eigen::Vector3f, std::vector<float>> process_blocks(const std::vector<Vector3f>& gridPoints,
                                                               const std::vector<Vector3f>& gridVelocities,
                                                               const Vector3f& min_pos, const Vector3f& max_pos,
                                                               const std::string& model_path,
-                                                              int split,int nx,int ny,int nz);
+                                                              int split,int nx,int ny,int nz,VolumeMesh::Pointer volume_Mesh,
+                                                              AttributeSet* attributeSet,
+                                                              int curIndex);
 
     ArrayObject::Pointer AttributeCell2Point(CellArray::Pointer Cell, ArrayObject::Pointer OriArray,
                                                           size_t PointNum);
