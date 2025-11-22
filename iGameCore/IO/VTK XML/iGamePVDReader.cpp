fa4cd03b0ec409606ece8db93bc6d2886ee7438e
mky8812 <2819787107@qq.com>
2025-11-22 21:31:41 +0800

    update vts、vtu strok's Thread Unsafe BUG

--- iGameCore/IO/VTK XML/iGamePVDReader.cpp
+++ iGameCore/IO/VTK XML/iGamePVDReader.cpp
@@ -75,8 +75,11 @@ bool iGame::iGamePVDReader::Parsing() {
                     DataObject::Pointer newObj;
                     const char* pos = strrchr(fileName.data(), '.');
                     std::string fileSuffix;
-                    const char *fileEnd = fileName.data() + fileName.size();
-                    fileSuffix = std::string(pos + 1, fileEnd);
+                    if (pos) {
+                        const char *fileEnd = fileName.data() + fileName.size();
+                        fileSuffix = std::string(pos + 1, fileEnd);
+                    }
+                    
                     if(fileSuffix == "vts"){
                         iGameVTSReader::Pointer rd = iGameVTSReader::New();
                         rd->SetFilePath(fileName);
