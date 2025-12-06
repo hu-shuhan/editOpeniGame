6e214a168c54b99d2aab8ee92a23c65830d83837
ryaznb <1025772790@qq.com>
2025-12-06 18:14:51 +0800

    up

--- Qt/src/IQCore/igQtMainWindow.cpp
+++ Qt/src/IQCore/igQtMainWindow.cpp
@@ -1213,6 +1213,8 @@ void igQtMainWindow::initAllMySignalConnections() {
 
     connect(ui->widget_FlowField, &igQtStreamTracerWidget::AddStreamObject, this, [&](iGame::DataObject::Pointer res) {
         modelTreeWidget->addDataObjectToModelTree(res, ItemSource::Algorithm);
+        auto scene = iGame::SceneManager::Instance()->GetCurrentScene();
+        scene->GetCurrentModel()->SetViewWireframeSwitch(true);
     });
     connect(ui->widget_FlowField, &igQtStreamTracerWidget::UpdateStreamObject, this,
             [&](iGame::DataObject::Pointer res) {
