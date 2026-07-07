fda37a43d70610fc9dd2c7405676eeddbd0f5ff3
南桑 <371603816@qq.com>
2026-07-07 14:16:23 +0800

    修改contour extract bug

--- Qt/include/IQWidgets/igQtContourExtractWidget.h
+++ Qt/include/IQWidgets/igQtContourExtractWidget.h
@@ -6,7 +6,7 @@
 #pragma once
 #include "Clip/iGameModelClip.h"
 #include "Contour/iGameContourFilter.h"
-#include "iGameSurfaceMesh.h"
+#include "iGameUnstructuredMesh.h"
 
 #include <ui_ContourExtract.h>
 class igQtContourExtractWidget : public QWidget {
@@ -43,13 +43,14 @@ private:
     Ui::ContourExtract* ui;
 
     iGame::DataObject::Pointer m_OriginDataObject{ nullptr };
-    iGame::SurfaceMesh::Pointer m_ResultMesh{ nullptr };
+    iGame::UnstructuredMesh::Pointer m_ResultMesh{ nullptr };
     iGame::ContourFilter::Pointer m_Extracter{ nullptr };
     iGame::ElementArray<iGame::AttributeSet::Attribute>::Pointer m_PointData=nullptr;
     iGame::ArrayObject::Pointer m_ScalarArray=nullptr;
-    int m_ScalarDimension=-1;
+    int m_ScalarDimension=0;
     std::string m_ScalarName="";
     bool m_Generated = false;
     double m_IsoValue=0.0;
+    unsigned long m_ResultObserverTag{0};
     
 };
