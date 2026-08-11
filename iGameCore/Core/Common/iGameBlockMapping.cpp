de07ef1450f84568aa5fc02877a727bf320a56e4
Kornephoros <2628025450@qq.com>
2026-08-11 20:36:54 +0800

    Update iGameBlockMapping.cpp

--- iGameCore/Core/Common/iGameBlockMapping.cpp
+++ iGameCore/Core/Common/iGameBlockMapping.cpp
@@ -1,5 +1,6 @@
 ﻿#include "iGameBlockMapping.h"
 #include <iGamePointFinder.h>
+#include <iGameThreadPool.h>
 #include <unordered_map>
 using namespace std;
 IGAME_NAMESPACE_BEGIN
@@ -38,43 +39,35 @@ std::vector<int> BlockMapping::GetMappingBlockCells(SurfaceMesh::Pointer oriMesh
 
     auto oriCellNum = oriMesh->GetNumberOfFaces();
     std::vector<int> result(oriCellNum, 0);
-    for (int cellId = 0; cellId < oriCellNum; cellId++) {
-        auto cell = oriMesh->GetFace(cellId);
-        int pointSize = cell->GetNumberOfPoints();
-        std::unordered_map<int, int> votes;
+    ThreadPool::parallelFor(0, static_cast<int>(oriCellNum), [&](int s, int e) {
+        igIndex ptIds[64];
+        for (int cellId = s; cellId < e; cellId++) {
+            int nPts = oriMesh->GetFacePointIds(cellId, ptIds);
+            std::unordered_map<int, int> votes;
 
-        Vector3d centroid(0.0, 0.0, 0.0);
-        for (int pi = 0; pi < pointSize; pi++) {
-            auto& p = cell->GetPoint(pi);
-            centroid[0] += p[0];
-            centroid[1] += p[1];
-            centroid[2] += p[2];
-
-            // 对每个顶点单独投票
-            Vector3d vp(p[0], p[1], p[2]);
-            igIndex nearest = finder->FindClosestPoint(vp);
-            votes[static_cast<int>(partIdArray->GetElementValue(nearest, 0))]++;
-        }
-
-        // 质心也参与投票
-        if (pointSize > 0) {
-            centroid[0] /= pointSize;
-            centroid[1] /= pointSize;
-            centroid[2] /= pointSize;
-            igIndex nearest = finder->FindClosestPoint(centroid);
-            votes[static_cast<int>(partIdArray->GetElementValue(nearest, 0))]++;
-        }
-
-        // 取票数最多的 part_id
-        int bestId = 0, bestCount = 0;
-        for (auto& [id, count] : votes) {
-            if (count > bestCount) {
-                bestCount = count;
-                bestId = id;
+            Vector3d centroid(0.0, 0.0, 0.0);
+            for (int pi = 0; pi < nPts; pi++) {
+                auto& p = oriMesh->GetPoint(ptIds[pi]);
+                centroid[0] += p[0];
+                centroid[1] += p[1];
+                centroid[2] += p[2];
+                Vector3d vp(p[0], p[1], p[2]);
+                votes[static_cast<int>(partIdArray->GetElementValue(finder->FindClosestPoint(vp), 0))]++;
             }
+            if (nPts > 0) {
+                centroid[0] /= nPts;
+                centroid[1] /= nPts;
+                centroid[2] /= nPts;
+                votes[static_cast<int>(partIdArray->GetElementValue(finder->FindClosestPoint(centroid), 0))]++;
+            }
+
+            int bestId = 0, bestCount = 0;
+            for (auto& [id, count] : votes) {
+                if (count > bestCount) { bestCount = count; bestId = id; }
+            }
+            result[cellId] = bestId;
         }
-        result[cellId] = bestId;
-    }
+    });
 
     return result;
 }
@@ -109,33 +102,35 @@ std::vector<int> BlockMapping::GetMappingBlockCells(UnstructuredMesh::Pointer or
 
     auto oriCellNum = oriMesh->GetNumberOfCells();
     std::vector<int> result(oriCellNum, 0);
-    for (IGsize cellId = 0; cellId < oriCellNum; cellId++) {
-        const igIndex* ptIds = nullptr;
-        int nPts = oriMesh->GetCellPointIds(cellId, ptIds);
-        std::unordered_map<int, int> votes;
+    ThreadPool::parallelFor(0, static_cast<int>(oriCellNum), [&](int s, int e) {
+        for (int cellId = s; cellId < e; cellId++) {
+            const igIndex* ptIds = nullptr;
+            int nPts = oriMesh->GetCellPointIds(cellId, ptIds);
+            std::unordered_map<int, int> votes;
 
-        Vector3d centroid(0.0, 0.0, 0.0);
-        for (int pi = 0; pi < nPts; pi++) {
-            auto& p = oriMesh->GetPoint(ptIds[pi]);
-            centroid[0] += p[0];
-            centroid[1] += p[1];
-            centroid[2] += p[2];
-            Vector3d vp(p[0], p[1], p[2]);
-            votes[static_cast<int>(partIdArray->GetElementValue(finder->FindClosestPoint(vp), 0))]++;
-        }
-        if (nPts > 0) {
-            centroid[0] /= nPts;
-            centroid[1] /= nPts;
-            centroid[2] /= nPts;
-            votes[static_cast<int>(partIdArray->GetElementValue(finder->FindClosestPoint(centroid), 0))]++;
-        }
+            Vector3d centroid(0.0, 0.0, 0.0);
+            for (int pi = 0; pi < nPts; pi++) {
+                auto& p = oriMesh->GetPoint(ptIds[pi]);
+                centroid[0] += p[0];
+                centroid[1] += p[1];
+                centroid[2] += p[2];
+                Vector3d vp(p[0], p[1], p[2]);
+                votes[static_cast<int>(partIdArray->GetElementValue(finder->FindClosestPoint(vp), 0))]++;
+            }
+            if (nPts > 0) {
+                centroid[0] /= nPts;
+                centroid[1] /= nPts;
+                centroid[2] /= nPts;
+                votes[static_cast<int>(partIdArray->GetElementValue(finder->FindClosestPoint(centroid), 0))]++;
+            }
 
-        int bestId = 0, bestCount = 0;
-        for (auto& [id, count] : votes) {
-            if (count > bestCount) { bestCount = count; bestId = id; }
+            int bestId = 0, bestCount = 0;
+            for (auto& [id, count] : votes) {
+                if (count > bestCount) { bestCount = count; bestId = id; }
+            }
+            result[cellId] = bestId;
         }
-        result[cellId] = bestId;
-    }
+    });
     return result;
 }
 
@@ -156,33 +151,35 @@ std::vector<int> BlockMapping::GetMappingBlockCells(VolumeMesh::Pointer oriMesh,
 
     auto oriCellNum = oriMesh->GetNumberOfVolumes();
     std::vector<int> result(oriCellNum, 0);
-    igIndex ptIds[64];
-    for (IGsize cellId = 0; cellId < oriCellNum; cellId++) {
-        int nPts = oriMesh->GetVolumePointIds(cellId, ptIds);
-        std::unordered_map<int, int> votes;
+    ThreadPool::parallelFor(0, static_cast<int>(oriCellNum), [&](int s, int e) {
+        igIndex ptIds[64];
+        for (int cellId = s; cellId < e; cellId++) {
+            int nPts = oriMesh->GetVolumePointIds(cellId, ptIds);
+            std::unordered_map<int, int> votes;
 
-        Vector3d centroid(0.0, 0.0, 0.0);
-        for (int pi = 0; pi < nPts; pi++) {
-            auto& p = oriMesh->GetPoint(ptIds[pi]);
-            centroid[0] += p[0];
-            centroid[1] += p[1];
-            centroid[2] += p[2];
-            Vector3d vp(p[0], p[1], p[2]);
-            votes[static_cast<int>(partIdArray->GetElementValue(finder->FindClosestPoint(vp), 0))]++;
-        }
-        if (nPts > 0) {
-            centroid[0] /= nPts;
-            centroid[1] /= nPts;
-            centroid[2] /= nPts;
-            votes[static_cast<int>(partIdArray->GetElementValue(finder->FindClosestPoint(centroid), 0))]++;
-        }
+            Vector3d centroid(0.0, 0.0, 0.0);
+            for (int pi = 0; pi < nPts; pi++) {
+                auto& p = oriMesh->GetPoint(ptIds[pi]);
+                centroid[0] += p[0];
+                centroid[1] += p[1];
+                centroid[2] += p[2];
+                Vector3d vp(p[0], p[1], p[2]);
+                votes[static_cast<int>(partIdArray->GetElementValue(finder->FindClosestPoint(vp), 0))]++;
+            }
+            if (nPts > 0) {
+                centroid[0] /= nPts;
+                centroid[1] /= nPts;
+                centroid[2] /= nPts;
+                votes[static_cast<int>(partIdArray->GetElementValue(finder->FindClosestPoint(centroid), 0))]++;
+            }
 
-        int bestId = 0, bestCount = 0;
-        for (auto& [id, count] : votes) {
-            if (count > bestCount) { bestCount = count; bestId = id; }
+            int bestId = 0, bestCount = 0;
+            for (auto& [id, count] : votes) {
+                if (count > bestCount) { bestCount = count; bestId = id; }
+            }
+            result[cellId] = bestId;
         }
-        result[cellId] = bestId;
-    }
+    });
     return result;
 }
 
