dcc041c4bcb7043065ef8383ea7e281a84a9889e
Kornephoros <2628025450@qq.com>
2026-08-19 21:15:37 +0800

    去掉烦人的cout

--- iGameCore/Core/Common/iGameThreadPool.h
+++ iGameCore/Core/Common/iGameThreadPool.h
@@ -54,7 +54,6 @@ public:
             return;
         }
         int chunkSize = (range + numThreads - 1) / numThreads;
-        std::cout << "The number of threads uesd  is " << numThreads << '\n';
         std::vector<std::future<void>> futures;
         for (int i = 0; i < numThreads; ++i) {
             int chunkStart = start + i * chunkSize;
@@ -62,7 +61,6 @@ public:
             int chunkEnd = (i == numThreads - 1) ? end : std::min(end, chunkStart + chunkSize);
             if (chunkStart == chunkEnd) { continue; }
             // 使用线程池提交任务
-            std::cout << chunkStart << " " << chunkEnd << " " << i << std::endl;
             futures.emplace_back(ThreadPool::Instance()->Commit([=]() { process(chunkStart, chunkEnd); }));
         }
         // 等待所有任务完成
@@ -96,7 +94,6 @@ public:
             return;
         }
         int chunkSize = (range + numThreads - 1) / numThreads;
-        std::cout << "The number of threads uesd  is " << numThreads << '\n';
         std::vector<std::thread> threads;
         for (int i = 0; i < numThreads; ++i) {
             int chunkStart = start + i * chunkSize;
@@ -134,7 +131,6 @@ public:
 
 private:
     ThreadPool(unsigned int num = std::thread::hardware_concurrency()) : stop_(false) {
-        std::cout << "Thread number: " << num << std::endl;
 #if IGAME_EMSCRIPTEN_SINGLE_THREAD
         thread_num_ = 1;
 #else
